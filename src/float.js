b47f8d072990cd06b8fb63930d5688d9a848a16c
Bonsanto <xbonsx@gmail.com>
2014-10-22 23:02:05 -0430

    Added semicolons and indentation.

--- src/float.js
+++ src/float.js
@@ -1,49 +1,49 @@
-var Float = function(value) {
+var Float = function (value) {
 
     //Si value es una string, lo convierte a float (number)
-    if(typeof value === 'string' || value instanceof String) {
-	value = parseFloat(value);
+    if (typeof value === 'string' || value instanceof String) {
+        value = parseFloat(value);
     }
 
-    this.compareTo = function(anotherFloat) {
-	if(value < anotherFloat) {
-	    return -1;
-	}
-	if(value > anotherFloat) {
-	    return 1;
-	}
-	else {
-	    return 0;
-	}
-    }
+    this.compareTo = function (anotherFloat) {
+        if (value < anotherFloat) {
+            return -1;
+        }
+        if (value > anotherFloat) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    };
 
-    this.floatValue = function() {
-	return value;
-    }
+    this.floatValue = function () {
+        return value;
+    };
 
     this.intValue = function () {
-	return parseInt(value, 10);
-    }
+        return parseInt(value, 10);
+    };
 
-    this.toString = function() {
-	return  value.toString();
+    this.toString = function () {
+        return  value.toString();
     }
-}
+};
 
 //Metodos estaticos de la clase...
-Float.compare = function(f1, f2) {
-    if(f1 < f2) {
-	return -1;
+Float.compare = function (f1, f2) {
+    if (f1 < f2) {
+        return -1;
     }
-    else if(f1 > f2) {
-	return 1;
+    else if (f1 > f2) {
+        return 1;
     }
     else {
-	return 0;
+        return 0;
     }
-}
+};
 
-Float.intBitsToFloat = function(bits) {
+Float.intBitsToFloat = function (bits) {
     var bitStr = bits.toString(2);
     var signStr;
     var exponentStr;
@@ -52,37 +52,37 @@ Float.intBitsToFloat = function(bits) {
     var exponent = 0;
     var len;
 
-    if(bitStr.length < 32) {
-	len = bitStr.length;
-	for(var i=0; i<32-len; i++) {
-	    bitStr = '0' + bitStr;
-	}
+    if (bitStr.length < 32) {
+        len = bitStr.length;
+        for (var i = 0; i < 32 - len; i++) {
+            bitStr = '0' + bitStr;
+        }
     }
 
     signStr = bitStr[0];
     exponentStr = bitStr.substr(1, 8);
     fractionStr = bitStr.substr(9, 31);
 
-    for(var i=0; i<8; i++) {
-	exponent += parseInt(exponentStr[7-i]) * Math.pow(2, i);
+    for (var i = 0; i < 8; i++) {
+        exponent += parseInt(exponentStr[7 - i]) * Math.pow(2, i);
     }
 
-    for(var i=0; i<23; i++) {
-	fraction += parseInt(fractionStr[i]) * Math.pow(2, -(i+1));
+    for (var i = 0; i < 23; i++) {
+        fraction += parseInt(fractionStr[i]) * Math.pow(2, -(i + 1));
     }
 
-    return Math.pow(-1, parseInt(signStr)) * (1+fraction) * Math.pow(2, exponent  - 127);
-}
+    return Math.pow(-1, parseInt(signStr)) * (1 + fraction) * Math.pow(2, exponent - 127);
+};
 
-Float.parseFloat = function(s) {
+Float.parseFloat = function (s) {
     return parseFloat(s);
-}
+};
 
-Float.toString = function(f) {
+Float.toString = function (f) {
     return f.toString();
 };
 
-Float.valueOf = function(f) {
+Float.valueOf = function (f) {
     return new Float(f);
 };
 
