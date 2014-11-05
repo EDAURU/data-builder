037780225607c27572647f54c3ea9bbabd5e4fe2
Andres Ortega <adresortegao@gmail.com>
2014-11-04 20:02:43 -0430

    Fix the compareTo method and added the floatToIntBits method.

--- src/Float.js
+++ src/Float.js
@@ -1,13 +1,13 @@
 var Float = function (value) {
-
+//Ahi
     //Si value es una string, lo convierte a float (number)
     if (typeof value === 'string' || value instanceof String) {
         value = parseFloat(value);
     }
 
     this.compareTo = function (anotherFloat) {
-        if (value < anotherFloat) return -1;
-        if (value > anotherFloat) return 1;
+        if (value < anotherFloat.floatValue) return -1;
+        if (value > anotherFloat.floatValue) return 1;
         else return 0;
     };
 
@@ -31,6 +31,44 @@ Float.compare = function (f1, f2) {
     else return 0;
 };
 
+Float.floatToIntBits = function(f) {
+    var bitStr = '';
+    var exponentStr = '';
+    var fractionStr = '';
+    var exponent = 0; 
+    var fraction = 0;
+    var len;
+
+    if (f >= 0) bitStr = '0';
+    else bitStr = '1';
+    
+    exponent = Math.round(Math.log(f) / Math.LN2);
+    //El exponente es sumado con 127 en la representacion IEEE754
+    exponentStr =  (exponent + 127).toString(2);
+    len = exponentStr.length;
+
+    if(len<8) {
+	for(var i=0; i<8-len; i++) {
+	    exponentStr = '0' + exponentStr;
+	}
+    }
+
+    fraction = f/Math.pow(2, exponent);
+    //Se toma solo la parte decimal
+    fractionStr = fraction.toString(2).split('.')[1];
+    len = fractionStr.length;
+
+    if(len<23) {
+	for(i=0; i<23-len; i++) {
+	    fractionStr += '0';
+	}
+    }
+
+    bitStr += exponentStr + fractionStr;
+    
+    return parseInt(bitStr, 2);
+};
+
 Float.intBitsToFloat = function (bits) {
     var bitStr = bits.toString(2);
     var signStr;
@@ -43,7 +81,7 @@ Float.intBitsToFloat = function (bits) {
     if (bitStr.length < 32) {
         len = bitStr.length;
         for (var i = 0; i < 32 - len; i++) {
-            bitStr = '0' + bitStr;
+           bitStr = '0' + bitStr;
         }
     }
 
