638247533df296ef61b66499615d433c691df1d4
JonnJJordan <johnjordan756@gmail.com>
2014-11-05 22:18:50 -0430

    charValue() method fixed and code optimized!

--- src/Char.js
+++ src/Char.js
@@ -1,22 +1,29 @@
 var Char = function (c) {
+    
+    var cCount;
+    var charLetter;
+	var pointCount;
+	var xChar;
+	var yChar;
+	var value;
 
     this.charCount = function (code) {
-        var cCount = parseInt(code);
+        cCount = parseInt(code);
         if (parseInt(cCount) < 10000)  return 1;
         else return 2;
     };
 
     this.charValue = function () {
-        return c;
+        return c.charAt(0);
     };
 
     this.codePointAt = function (charArray, index) {
-        var charLetter = charArray[index];
+        charLetter = charArray[index];
         return charLetter.charCodeAt(0);
     };
 
     this.codePointBefore = function (charArray, index) {
-        var charLetter = charArray[index - 1];
+        charLetter = charArray[index - 1];
         return charLetter.charCodeAt(0);
     };
 
@@ -25,22 +32,22 @@ var Char = function (c) {
     };
 
     this.compare = function (x, y) {
-        var xChar = x.charValue().charCodeAt(0);
-        var yChar = y.charValue().charCodeAt(0);
+        xChar = x.charValue().charCodeAt(0);
+        yChar = y.charValue().charCodeAt(0);
 
         return (xChar === yChar) ? 0 : (xChar < yChar) ? -1 : 1;
     };
 
     this.compareTo = function (x) {
-        var value = this.charValue().charCodeAt(0);
-        var xChar = x.charValue().charCodeAt(0);
+        value = this.charValue().charCodeAt(0);
+        xChar = x.charValue().charCodeAt(0);
 
         return (value === xChar) ? 0 : (value < xChar) ? -1 : 1;
     };
 
     this.equals = function (x) {
-        var value = this.charValue().charCodeAt(0);
-        var xChar = x.charValue().charCodeAt(0);
+        value = this.charValue().charCodeAt(0);
+        xChar = x.charValue().charCodeAt(0);
         return value === xChar;
     };
 
@@ -53,37 +60,37 @@ var Char = function (c) {
     };
 
     this.isDefined = function (x) {
-        var xChar = x.charCodeAt(0);
+        xChar = x.charCodeAt(0);
         return (xChar >= 0 && xChar <= 127);
     };
 
     this.isLetter = function (x) {
-        var xChar = x.charCodeAt(0);
+        xChar = x.charCodeAt(0);
         return (xChar >= 65 && xChar <= 122);
     };
 
     this.isDigit = function (x) {
-        var xChar = x.charCodeAt(0);
+        xChar = x.charCodeAt(0);
         return xChar >= 48 && xChar <= 57;
     };
 
     this.isLetterOrDigit = function (x) {
-        var xChar = x.charCodeAt(0);
+        xChar = x.charCodeAt(0);
         return (xChar >= 48 && xChar <= 57) || (xChar >= 65 && xChar <= 122);
     };
 
     this.isLowerCase = function (x) {
-        var xChar = x.charCodeAt(0);
+        xChar = x.charCodeAt(0);
         return (xChar >= 97 && xChar <= 122);
     };
 
     this.isUpperCase = function (x) {
-        var xChar = x.charCodeAt(0);
+        xChar = x.charCodeAt(0);
         return (xChar >= 65 && xChar <= 90);
     };
 
     this.toLowerCase = function (x) {
-        var xChar = x.charValue().charCodeAt(0);
+        xChar = x.charValue().charCodeAt(0);
         if (xChar >= 65 && xChar <= 90) return String.fromCharCode(xChar + 32);
     };
 
@@ -92,7 +99,7 @@ var Char = function (c) {
     };
 
     this.toUpperCase = function (x) {
-        var xChar = x.charValue().charCodeAt(0);
+        xChar = x.charValue().charCodeAt(0);
         if (xChar >= 97 && xChar <= 122) return String.fromCharCode(xChar - 32);
     };
 
@@ -100,6 +107,3 @@ var Char = function (c) {
         return this.charValue().valueOf();
     };
 };
-
-var porn = new Char('porn');
-console.log(porn.charValue()); //TODO: SHOULD THROW AN EXCEPTION WHEN TRYING TO SET A CHAR WITH A STRING OF MORE THAN 1 ELEMENT, AND SHOULD RETURN ONLY ONE CHAR WHEN THIS IS DONE.
